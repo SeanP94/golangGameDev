00b4a0c7ced34c643a6bb5480b1b2915655ab2c0
Sean Perry <Seanep94@gmail.com>
2024-12-27 16:42:40 -0800

    Added some more practice code, Most importantly, I did some code to bring in a sprite and some controls

--- tutorial/main.go
+++ tutorial/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image"
 	"image/color"
 	"log"
 
@@ -13,17 +14,42 @@ var WINDOW_WIDTH = 320
 var WINDOW_HEIGHT = 240
 
 // Engine object.
-type Game struct{}
+type Game struct {
+	PlayerImage *ebiten.Image
+	X, Y        float64
+}
 
 func (g *Game) Update() error {
+
+	if ebiten.IsKeyPressed((ebiten.KeyRight)) {
+		g.X += 2
+	}
+	if ebiten.IsKeyPressed((ebiten.KeyLeft)) {
+		g.X -= 2
+	}
+	if ebiten.IsKeyPressed((ebiten.KeyUp)) {
+		g.Y -= 2
+	}
+	if ebiten.IsKeyPressed((ebiten.KeyDown)) {
+		g.Y += 2
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-
 	screen.Fill(color.RGBA{120, 180, 255, 255})
+
+	opts := ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(g.X, g.Y)
+
+	screen.DrawImage(
+		g.PlayerImage.SubImage(
+			image.Rect(0, 0, 16, 16),
+		).(*ebiten.Image), // image.Rect can cast to a *ebiten.Image :)
+		&opts,
+	)
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -35,7 +61,12 @@ func main() {
 	ebiten.SetWindowTitle("Hello, World!")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	playerImg, _, err := ebitenutil.NewImageFromFile("assets/images/knight.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := ebiten.RunGame(&Game{PlayerImage: playerImg, X: 100, Y: 100}); err != nil {
 		log.Fatal(err)
 	}
 }
