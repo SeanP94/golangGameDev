953486cb9854896300bd11545395766382b32b57
Sean <seanep94@gmail.com>
2025-01-15 08:37:37 -0800

    Shaders are actually working kind of now, need to figure out how to make them work interactively

--- rayliblearn/main.go
+++ rayliblearn/main.go
@@ -5,19 +5,23 @@ import rl "github.com/gen2brain/raylib-go/raylib"
 func main() {
 	rl.InitWindow(400, 400, "Hello Window")
 	rl.SetTargetFPS(60)
+
+	// Canvas for the shader
+	blank := rl.GenImageColor(1024, 1024, rl.Blank)
+	texture := rl.LoadTextureFromImage(blank)
+	shader := drawShader()
+	rl.UnloadImage(blank)
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Hello World!", 20, 200, 20, rl.Black)
-
-		shader := drawShader()
-
 		rl.BeginShaderMode(shader)
+		rl.DrawTexture(texture, 0, 0, rl.Blank)
 
 		rl.EndShaderMode()
 		rl.EndDrawing()
 	}
 
 	rl.CloseWindow()
-
 }
